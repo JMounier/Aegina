884afb668e214c30f0163ce902780e394f1766fd
Florian Amsallem <Florian Amsallem>
2015-11-16 12:54:11 +0100

    Update CameraTrack

--- Assets/Scripts/CameraTrack.cs
+++ Assets/Scripts/CameraTrack.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class CameraTrack : MonoBehaviour {
 
@@ -9,21 +10,35 @@ public class CameraTrack : MonoBehaviour {
     public float DistanceMax;
     public float AngleXMin;
     public float AngleXMax;
+
+    private Vector3 MousePosition;
     
     // Use this for initialization
     void Start ()
     {
         Vector3 positionPersonnage = this.Personnage.transform.position;
-        Vector3 positionCamera = new Vector3(positionPersonnage.x, positionPersonnage.y, positionPersonnage.z - this.Distance);
+        Vector3 positionCamera = new Vector3(positionPersonnage.x, positionPersonnage.y, positionPersonnage.z + this.Distance);
         this.transform.position = positionCamera;
+        this.transform.LookAt(this.Personnage.transform);
 
-        Quaternion angle = new Quaternion(0,0,0,0);
-        this.transform.rotation = angle;
-	}
+        this.MousePosition = Input.mousePosition;
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    
-	}
+        this.Distance -= Input.mouseScrollDelta.y / 10;
+        Vector3 deltaMouse = this.MousePosition - Input.mousePosition;
+        this.MousePosition = Input.mousePosition;
+        this.transform.Translate(deltaMouse.x, deltaMouse.y, 0);
+
+        this.transform.LookAt(this.Personnage.transform);
+
+        Vector3 posPersonnage = this.Personnage.transform.position;
+        Vector3 posCamera = this.transform.position;
+
+        float distance = (float) Math.Sqrt(Math.Pow(posPersonnage.x - posCamera.x, 2) + Math.Pow(posPersonnage.y - posCamera.y, 2) + Math.Pow(posPersonnage.z - posCamera.z, 2));
+        this.transform.Translate(0, 0, distance - this.Distance);
+    }
 }
