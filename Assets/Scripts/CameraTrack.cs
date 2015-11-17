9f2a682d34acdbbae6c9aa7bef2a6e77bdc9bfab
Florian Amsallem <Florian Amsallem>
2015-11-17 15:35:43 +0100

    Finish CameraTrack script and delete all test elements

--- Assets/Scripts/CameraTrack.cs
+++ Assets/Scripts/CameraTrack.cs
@@ -13,36 +13,47 @@ public class CameraTrack : MonoBehaviour {
     public float Sensibility;
     public float SensibilityScroll;
 
-    private Vector3 MousePosition;    
+    private Vector3 MousePosition;
 
     // Use this for initialization
     void Start ()
-    {
-        Vector3 positionPersonnage = this.Personnage.transform.position;
-        Vector3 positionCamera = new Vector3(positionPersonnage.x, positionPersonnage.y, positionPersonnage.z + this.Distance);
-        this.transform.position = positionCamera;
-        this.transform.LookAt(this.Personnage.transform);
-
+    {      
         this.MousePosition = Input.mousePosition;
-
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        // Get new distance
         this.Distance -= Input.mouseScrollDelta.y * this.SensibilityScroll;
+        this.Distance = Boundary(this.Distance, this.DistanceMin, this.DistanceMax);
+
+        // Get the drag 
         Vector3 deltaMouse = this.MousePosition - Input.mousePosition;
         deltaMouse *= this.Sensibility * this.Distance;
-
         this.MousePosition = Input.mousePosition;
-        this.transform.Translate(deltaMouse.x, deltaMouse.y, 0);
+        
+        float angleX = (this.transform.rotation.eulerAngles.x + 180) % 360;
+        // Moove the camera
+        if ((angleX < AngleXMin && deltaMouse.y < 0) || (angleX > AngleXMax && deltaMouse.y > 0))
+           this.transform.Translate(deltaMouse.x, 0, 0);
+        else
+            this.transform.Translate(deltaMouse.x, deltaMouse.y, 0);
 
         this.transform.LookAt(this.Personnage.transform);
 
+        // Adjust the distance
         Vector3 posPersonnage = this.Personnage.transform.position;
         Vector3 posCamera = this.transform.position;
-
         float distance = (float) Math.Sqrt(Math.Pow(posPersonnage.x - posCamera.x, 2) + Math.Pow(posPersonnage.y - posCamera.y, 2) + Math.Pow(posPersonnage.z - posCamera.z, 2));
         this.transform.Translate(0, 0, distance - this.Distance);
     }
+
+    // Check if the value is under or upper the limits
+    private float Boundary(float val, float floor, float ceil)
+    {
+        if (val > ceil) return ceil;
+        else if (val < floor) return floor;
+        else return val;
+    }       
 }
