a26e43be683445ff906b7c4c36b646947ed087a2
Florian Amsallem <Florian Amsallem>
2015-11-18 20:05:17 +0100

    Add FPS mode and rectify TPS mode

--- Assets/Scripts/CameraTrack.cs
+++ Assets/Scripts/CameraTrack.cs
@@ -5,55 +5,62 @@ using System;
 public class CameraTrack : MonoBehaviour {
 
     public GameObject Personnage;    
-    public float Distance;
-    public float DistanceMin;
-    public float DistanceMax;
-    public float AngleXMin;
-    public float AngleXMax;
-    public float Sensibility;
-    public float SensibilityScroll;
+    public float Distance = 5;
+    public float DistanceMin = 0.01f;
+    public float DistanceMax = 10;
+    public float YMin = 0.15f;
+    public float YMax = 1;
+    public float YMinFPS = -60;
+    public float YMaxFPS = 60;
+    public float Sensitivity = 0.1f;
+    public float SensitivityScroll = 0.75f;
 
-    private Vector3 MousePosition;
-
-    // Use this for initialization
-    void Start ()
-    {      
-        this.MousePosition = Input.mousePosition;
-    }
+    private float rotationY = 0F;
+    private float rotationX = 0F;
 	
 	// Update is called once per frame
 	void Update ()
     {
         // Get new distance
-        this.Distance -= Input.mouseScrollDelta.y * this.SensibilityScroll;
-        this.Distance = Boundary(this.Distance, this.DistanceMin, this.DistanceMax);
+        this.Distance -= Input.mouseScrollDelta.y * this.SensitivityScroll;
+        this.Distance = Mathf.Clamp(this.Distance, this.DistanceMin, this.DistanceMax);
 
-        // Get the drag 
-        Vector3 deltaMouse = this.MousePosition - Input.mousePosition;
-        deltaMouse *= this.Sensibility * this.Distance;
-        this.MousePosition = Input.mousePosition;
-        
-        float angleX = (this.transform.rotation.eulerAngles.x + 180) % 360;
-        // Moove the camera
-        if ((angleX < AngleXMin && deltaMouse.y < 0) || (angleX > AngleXMax && deltaMouse.y > 0))
-           this.transform.Translate(deltaMouse.x, 0, 0);
+        // TPS
+        if (this.Distance > this.DistanceMin)
+        {
+            // Set value
+            Vector3 posPersonnage = this.Personnage.transform.position;
+            Vector3 posCamera = this.transform.position;
+            
+            // Get the drag 
+            Vector2 deltaMouse = new Vector2(-Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"));
+            deltaMouse *= this.Sensitivity * this.Distance;
+
+            // Moove the camera
+            if (posCamera.y + deltaMouse.y > this.YMax * this.Distance)
+                this.transform.Translate(deltaMouse.x, this.YMax * this.Distance - posCamera.y, 0);
+            else if (posCamera.y + deltaMouse.y < this.YMin)
+                this.transform.Translate(deltaMouse.x, this.YMin - posCamera.y, 0);
+            else
+                this.transform.Translate(deltaMouse.x, deltaMouse.y, 0);
+
+            this.transform.LookAt(this.Personnage.transform);
+
+            // Adjust the distance       
+            float distance = (float)Math.Sqrt(Math.Pow(posPersonnage.x - posCamera.x, 2) + Math.Pow(posPersonnage.y - posCamera.y, 2) + Math.Pow(posPersonnage.z - posCamera.z, 2));
+            this.transform.Translate(0, 0, distance - this.Distance);
+        }
+
+        // FPS
         else
-            this.transform.Translate(deltaMouse.x, deltaMouse.y, 0);
+        {
+            this.transform.position = this.Personnage.transform.position;
+            rotationX += Input.GetAxis("Mouse X") * Sensitivity * 100;
 
-        this.transform.LookAt(this.Personnage.transform);
+            rotationY += Input.GetAxis("Mouse Y") * Sensitivity * 100;
+            rotationY = Mathf.Clamp(rotationY, YMinFPS, YMaxFPS);
 
-        // Adjust the distance
-        Vector3 posPersonnage = this.Personnage.transform.position;
-        Vector3 posCamera = this.transform.position;
-        float distance = (float) Math.Sqrt(Math.Pow(posPersonnage.x - posCamera.x, 2) + Math.Pow(posPersonnage.y - posCamera.y, 2) + Math.Pow(posPersonnage.z - posCamera.z, 2));
-        this.transform.Translate(0, 0, distance - this.Distance);
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);            
+        }               
     }
-
-    // Check if the value is under or upper the limits
-    private float Boundary(float val, float floor, float ceil)
-    {
-        if (val > ceil) return ceil;
-        else if (val < floor) return floor;
-        else return val;
-    }       
 }
