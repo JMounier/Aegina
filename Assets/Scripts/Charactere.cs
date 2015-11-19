a656bb29e88121a9eed80f9f830f717f7d459137
issarn_t <issarn_t>
2015-11-19 18:14:12 +0100

    First try

---
+++ Assets/Scripts/Charactere.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Charactere : MonoBehaviour {
+
+    protected int Health;
+    public GameObject Camera;
+    public int HealthMax = 500;
+    public float MoveSpeed = 1f;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.Health = this.HealthMax;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {        
+        bool foward = Input.GetButton("foward");
+        bool back = Input.GetButton("back");
+        if (foward)
+        {
+            this.transform.Translate(0, 0, this.MoveSpeed, Camera.transform);
+        }
+        else if (back)
+        {
+            this.transform.Translate(0, 0, -this.MoveSpeed, Camera.transform);
+        }
+    }
+}
