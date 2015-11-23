853c712227a8d08b1835a15796f6de8eaf7e6c55
MrColorado <issarn_t@epita.fr>
2015-11-23 13:50:28 +0100

    deplacement et debut du saut

--- Assets/Scripts/Charactere.cs
+++ Assets/Scripts/Charactere.cs
@@ -1,32 +1,101 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Charactere : MonoBehaviour {
+public class Charactere : MonoBehaviour
+{
 
     protected int Health;
+    protected int Hunger;
+
+    private bool IsJumping = false;
+    private Vector3 LastPosition;
     public GameObject Camera;
     public int HealthMax = 500;
+    public int HungerMax = 100;
     public float MoveSpeed = 1f;
-    
+    public float JumpForce = 2f;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
+        this.Hunger = this.HungerMax;
         this.Health = this.HealthMax;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {        
-        bool foward = Input.GetButton("foward");
-        bool back = Input.GetButton("back");
-        if (foward)
+        this.LastPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool forward = Input.GetButton("Forward");
+        bool back = Input.GetButton("Back");
+        bool right = Input.GetButton("Right");
+        bool left = Input.GetButton("Left");
+        bool jump = Input.GetButton("Jump");
+
+        if (jump && !IsJumping)
         {
-            this.transform.Translate(0, 0, this.MoveSpeed, Camera.transform);
+            this.GetComponent<Rigidbody>().AddForce(0, JumpForce, 0);
+            this.IsJumping = true;
         }
-        else if (back)
+        if (IsJumping)
         {
-            this.transform.Translate(0, 0, -this.MoveSpeed, Camera.transform);
+            if (this.LastPosition.y == this.transform.position.y)
+            {
+                this.IsJumping = false;
+            }            
         }
+
+            float angle = Mathf.Deg2Rad * (360 - this.Camera.transform.rotation.eulerAngles.y);
+        if ((right && left) || (!right && !left))
+        {
+            if (forward && !back)
+            {
+                this.transform.Translate(-Mathf.Sin(angle) * Time.deltaTime * this.MoveSpeed, 0, Mathf.Cos(angle) * Time.deltaTime * this.MoveSpeed, Space.World);
+            }
+            else if (back && !forward)
+            {
+                this.transform.Translate(Mathf.Sin(angle) * Time.deltaTime * this.MoveSpeed, 0, -Mathf.Cos(angle) * Time.deltaTime * this.MoveSpeed, Space.World);
+            }
+        }
+        else if ((forward && back) || (!forward && !back))
+        {
+            angle += Mathf.PI / 2;
+            if (right && !left)
+            {
+                this.transform.Translate(Mathf.Sin(angle) * Time.deltaTime * this.MoveSpeed, 0, -Mathf.Cos(angle) * Time.deltaTime * this.MoveSpeed, Space.World);
+            }
+            else if (!right && left)
+            {
+                this.transform.Translate(-Mathf.Sin(angle) * Time.deltaTime * this.MoveSpeed, 0, Mathf.Cos(angle) * Time.deltaTime * this.MoveSpeed, Space.World);
+            }
+        }
+        else if (forward)
+        {
+            if (right)
+            {
+                angle -= Mathf.PI / 4;
+                this.transform.Translate(-Mathf.Sin(angle) * Time.deltaTime * this.MoveSpeed, 0, Mathf.Cos(angle) * Time.deltaTime * this.MoveSpeed, Space.World);
+            }
+            else
+            {
+                angle += Mathf.PI / 4;
+                this.transform.Translate(-Mathf.Sin(angle) * Time.deltaTime * this.MoveSpeed, 0, Mathf.Cos(angle) * Time.deltaTime * this.MoveSpeed, Space.World);
+            }
+        }
+        else
+        {
+            if (right)
+            {
+                angle += Mathf.PI / 4;
+                this.transform.Translate(Mathf.Sin(angle) * Time.deltaTime * this.MoveSpeed, 0, -Mathf.Cos(angle) * Time.deltaTime * this.MoveSpeed, Space.World);
+            }
+            else
+            {
+                angle -= Mathf.PI / 4;
+                this.transform.Translate(Mathf.Sin(angle) * Time.deltaTime * this.MoveSpeed, 0, -Mathf.Cos(angle) * Time.deltaTime * this.MoveSpeed, Space.World);
+            }
+        }
+
+        this.LastPosition = this.transform.position;
     }
 }
