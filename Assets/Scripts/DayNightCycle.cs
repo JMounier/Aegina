b75d789dac900d885cf457693bf8e6ec9db17237
flomonster <florian.amsallem@hotmail.fr>
2015-11-28 22:47:06 +0100

    Fin du Script mais rendu bof, de plus les skybox sont dupliquer sur 180deg...

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -1,110 +1,112 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DayNightCycle : MonoBehaviour {
+public class DayNightCycle : MonoBehaviour
+{
 
     public int CycleTime = 1000;
     public float RatioDayNight = 0.5f;
     public Cubemap[] ListCubemap = new Cubemap[20];
-    public Material SkyboxMaterial;
+    public Material MaterialSkybox;
 
     private float Actual_time;
 
-
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         this.Actual_time = 0f;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate()
-    {
+    }
 
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         this.Actual_time += Time.deltaTime; // avancement de temps
-        
+
         // changement de cycle
         if (this.Actual_time >= this.CycleTime)
             this.Actual_time -= this.CycleTime;
-        // calcul du pourcentage
-        // TimePercent = (int) this.Actual_time/(this.CycleTime)* 100;
 
         // changement de skybox
         int half = (int)(this.CycleTime * this.RatioDayNight);
         if (this.Actual_time <= half) // jour
         { // Ho - L - M - Hi - P - Hi - M - L - SS - T
             int day = half / 10;
-            if (this.Actual_time <= day)
-            // Ho
+            if (this.Actual_time <= day)                  
             {
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[0]);
-                print("Horizon");
+                // Ho      
+                this.MaterialSkybox.SetInt("_Rotation", 0);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[0]);
             }
             else if (this.Actual_time <= day * 2)
-                // L
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[1]);
+                // L            
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[1]);
             else if (this.Actual_time <= day * 3)
                 // M 
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[2]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[2]);
             else if (this.Actual_time <= day * 4)
                 // Hi
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[3]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[3]);
             else if (this.Actual_time <= day * 5)
                 // P
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[4]);
-            else if (this.Actual_time <= day * 6)
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[4]);
+            else if (this.Actual_time <= day * 6)            
+            {
                 // Hi
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[5]);
+                this.MaterialSkybox.SetInt("_Rotation", 180);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[5]);
+            }
             else if (this.Actual_time <= day * 7)
                 // M 
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[6]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[6]);
             else if (this.Actual_time <= day * 8)
                 // L
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[7]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[7]);
             else if (this.Actual_time <= day * 9)
                 // SS 
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[8]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[8]);
             else
                 // T
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[9]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[9]);
         }
         else  //nuit
         { // R - L - M - H - P - H - M - L - R - S
-            int night = (this.CycleTime-half)/10;
-            if (this.Actual_time <= night)
+            int night = (this.CycleTime - half) / 10;
+            if (this.Actual_time <= night)            
                 // R
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[10]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[10]);            
             else if (this.Actual_time <= half + night * 2)
                 // L
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[11]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[11]);
             else if (this.Actual_time <= half + night * 3)
                 // M 
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[12]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[12]);
             else if (this.Actual_time <= half + night * 4)
                 // H
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[13]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[13]);
             else if (this.Actual_time <= half + night * 5)
                 // P
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[14]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[14]);
             else if (this.Actual_time <= half + night * 6)
+            {
                 // H
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[15]);
+                this.MaterialSkybox.SetInt("_Rotation", 0);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[15]);
+            }
             else if (this.Actual_time <= half + night * 7)
                 // M 
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[16]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[16]);
             else if (this.Actual_time <= half + night * 8)
                 // L
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[17]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[17]);
             else if (this.Actual_time <= half + night * 9)
                 // R 
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[18]);
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[18]);
             else
                 // S
-                this.SkyboxMaterial.SetTexture("_Cube", ListCubemap[19]);
-
+                this.MaterialSkybox.SetTexture("_Tex", this.ListCubemap[19]);
         }
-	
-	}
+
+    }
     public float GetTime()
     {
         return this.Actual_time;
