d7ea9398bb6904fc30c8424e1cab52b1069b2c36
Florian Amsallem <Florian Amsallem>
2016-01-05 16:25:00 +0100

    debut de l'heritage des Items

--- Assets/Resources/Scripts/Item.cs
+++ Assets/Resources/Scripts/Item.cs
@@ -1,101 +1,108 @@
-﻿ using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-[System.Serializable]
-public class Item {
+/// <summary>
+///  Use it to create a new simple Item.
+/// </summary>
+public class Item
+{
+    protected int iD;
+    protected string name;
+    protected string description;
+    protected Texture2D icon;
+    protected int size;
 
-    private int maxquantity;
-    private string itemname;
-    private int iD;
-    private string description;
-    public Texture2D itemicon;
-    private int attackpower;
-    private int mineEfficiency;
-    private int axeEfficiency;
-    private ItemStack_Type itemType;
-    public enum ItemStack_Type
-    {
-        Arme, Consommable, Ressource, Armure, Outils
-    }
-
-
-    public Item(string name, int id, string desc,int maxquantity, int attackpower, int MineEfficiency, int axeEfficiency, ItemStack_Type type)
-    {
-        this.itemname = name;
-        this.iD = id;
-        this.description = desc;
-        this.attackpower = attackpower;
-        this.itemicon = Resources.Load<Texture2D>("Sprites/ItemIcons/"+ itemname);
-        this.mineEfficiency = MineEfficiency;
-        this.axeEfficiency = axeEfficiency;
-        this.itemType = type;
-        this.maxquantity = maxquantity;
-    }
+    // Constructors
     public Item()
     {
-        this.mineEfficiency = 1;
-        this.axeEfficiency = 1;
-        this.attackpower = 1;
-        this.iD = -1;
+        size = 0;
+        name = "";
+        iD = -1;
+        description = "";
+        icon = null;
     }
-    public int Maxquantity
+
+    public Item(int id, string name, string description, int size)
     {
-        get { return maxquantity; }
+        this.name = name;
+        this.iD = id;
+        this.description = description;
+        this.size = size;
+        this.icon = Resources.Load<Texture2D>("Sprites/ItemIcons/" + name);
     }
+
+    public Item(int id, string name, string description, int size, Texture2D icon)
+    {
+        this.name = name;
+        this.iD = id;
+        this.description = description;
+        this.size = size;
+        this.icon = icon;
+    }
+
+    // Getter & Setters
+    public int ID
+    {
+        get { return this.iD; }
+        set { this.iD = value; }
+    }
+
     public string Name
     {
-        get { return itemname; }
+        get { return this.name; }
+        set { this.name = value; }
     }
-    public int id
-    {
-        get { return iD; }
-    }
+
     public string Description
     {
-        get { return description; }
+        get { return this.description; }
+        set { this.description = value; }
     }
-    public int Attackpower
+
+    public Texture2D Icon
     {
-        get { return attackpower; }
+        get { return this.icon; }
+        set { this.icon = value; }
     }
-    public int MineEfficiency
+
+    public int Size
     {
-        get { return mineEfficiency; }
-    }
-    public int AxeEfficiency
-    {
-        get { return axeEfficiency; }
-    }
-    public ItemStack_Type Type
-    {
-        get { return itemType; }
+        get { return this.size; }
+        set { this.size = value; }
     }
 }
+
+/// <summary>
+///  Use it to create a group of a same Item.
+/// </summary>
 public class ItemStack
 {
     private Item items;
     private int quantity;
-    public ItemStack(Item prefab, int quantity)
-    {
-        this.items = prefab;
-        this.quantity = quantity;
-    }
 
+    // Constructors
     public ItemStack()
     {
         this.items = new Item();
-        this.quantity =0;
+        this.quantity = 0;
     }
-    public void Add(int nombre)
+
+    public ItemStack(Item items, int quantity)
     {
-        this.quantity += nombre;
+        this.items = items;
+        this.quantity = Mathf.Clamp(quantity, 0, items.Size);
     }
+
+    // Getter & Setters
+
     public int Quantity
     {
-        get { return quantity; }
+        get { return this.quantity; }
+        set { this.quantity = Mathf.Clamp(value, 0, items.Size); }
     }
-    public Item Item
+
+    public Item Items
     {
-        get { return items; }
+        get { return this.items; }
     }
 }
---
+++ Assets/Resources/Scripts/Tool.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+///  Use it to create a new commun Tool.
+/// </summary>
+public class Tool : Item
+{
+    protected int durability;
+    protected int maxDurability;
+    protected int damage;
+    protected int mineEfficiency;
+    protected int axeEfficiency;
+    protected int demolitionEfficiency;
+
+    // Constructors
+    public Tool () : base()
+    {
+        this.durability = 0;
+        this.maxDurability = 0;
+        this.damage = 0;
+        this.mineEfficiency = 0;
+        this.axeEfficiency = 0;
+        this.demolitionEfficiency = 0;
+    }
+
+    public Tool(int id, string name, string description, int durability, int damage, int mineEfficiency, int axeEfficiency, int demolitionEfficiency) : 
+        base(id, name, description, 1)
+    {
+        this.durability = durability;
+        this.maxDurability = durability;
+        this.damage = damage;
+        this.mineEfficiency = mineEfficiency;
+        this.axeEfficiency = axeEfficiency;
+        this.demolitionEfficiency = demolitionEfficiency; 
+    }
+
+    public Tool(int id, string name, string description, int durability, int damage, int mineEfficiency, int axeEfficiency, int demolitionEfficiency, Texture2D icon) : 
+        base(id, name, description, 1, icon)
+    {
+        this.durability = durability;
+        this.maxDurability = durability;
+        this.damage = damage;
+        this.mineEfficiency = mineEfficiency;
+        this.axeEfficiency = axeEfficiency;
+        this.demolitionEfficiency = demolitionEfficiency;
+    }
+
+    // Getter & Setters
+
+    public int Durability
+    {
+        get { return this.durability; }
+        set { this.durability = value; }
+    }
+
+    public int MaxDurability
+    {
+        get { return this.maxDurability; }
+        set { this.maxDurability = value; }
+    }
+
+    public int Damage
+    {
+        get { return this.damage; }
+        set { this.damage = value; }
+    }
+
+    public int MineEfficiency
+    {
+        get { return this.mineEfficiency; }
+        set { this.mineEfficiency = value; }
+    }
+
+    public int AxeEfficiency
+    {
+        get { return this.axeEfficiency; }
+        set { this.axeEfficiency = value; }
+    }
+
+    public int DemolitionEfficiency
+    {
+        get { return this.demolitionEfficiency; }
+        set { this.demolitionEfficiency = value; }
+    }
+}
+
+/// <summary>
+///  Use it to create a new Axe.
+/// </summary>
+public class Axe : Tool
+{
+    // Constructors
+    public Axe() : base() { }
+
+    public Axe(int id, string name, string description, int durability, int efficiency) :
+        base(id, name, description, durability, 5, 1, efficiency,  1) { }
+
+    public Axe(int id, string name, string description, int durability, int efficiency, Texture2D icon) :
+       base(id, name, description, durability, 5, 1, efficiency, 1, icon) { }
+}
+
+/// <summary>
+///  Use it to create a new Pickaxe.
+/// </summary>
+public class Pickaxe : Tool
+{
+    // Constructors
+    public Pickaxe() : base() { }
+
+    public Pickaxe(int id, string name, string description, int durability, int efficiency) :
+        base(id, name, description, durability, 3, efficiency, 1, 1) { }
+
+    public Pickaxe(int id, string name, string description, int durability, int efficiency, Texture2D icon) :
+       base(id, name, description, durability, 3, efficiency, 1, 1, icon) { }
+}
+
+/// <summary>
+///  Use it to create a new Sword.
+/// </summary>
+public class Sword : Tool
+{
+    // Constructors
+    public Sword() : base() { }
+
+    public Sword(int id, string name, string description, int durability, int damage) :
+        base(id, name, description, durability, damage, 1, 1, 1) { }
+
+    public Sword(int id, string name, string description, int durability, int damage, Texture2D icon) :
+       base(id, name, description, durability, damage, 1, 1, 1, icon) { }
+}
