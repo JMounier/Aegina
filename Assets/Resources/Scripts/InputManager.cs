7c3c87d7ab008b0302eda77db5387a2ebd81b059
nokiiam <romain.mounier@orange.fr>
2016-01-14 16:56:54 +0100

    ALELUYA tout peut enfin etre merge

--- Assets/Resources/Scripts/InputManager.cs
+++ Assets/Resources/Scripts/InputManager.cs
@@ -1,20 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
 
-public class InputManager : MonoBehaviour {
+public class InputManager : NetworkBehaviour {
     private Inventory inventaire;
     private Menu menu;
 
 	// Use this for initialization
 	void Start () {
+        if (!isLocalPlayer)
+            return;
         inventaire = GetComponentInParent<Inventory>();
         menu = GetComponentInParent<Menu>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (Input.GetButtonDown("Inventory") && !menu.menuShown && !menu.optionShown)
+        if (!isLocalPlayer)
+            return;
+        if (Input.GetButtonDown("Inventory") && !menu.MenuShown && !menu.OptionShown)
         {
             inventaire.InventoryShown = !inventaire.InventoryShown;
         }
@@ -23,13 +28,61 @@ public class InputManager : MonoBehaviour {
         {
             if (inventaire.InventoryShown)
                 inventaire.InventoryShown = false;
-            else if (menu.optionShown)
+            else if (menu.OptionShown)
             {
-                menu.optionShown = false;
-                menu.menuShown = true;
+                menu.OptionShown = false;
+                menu.MenuShown = true;
+            }
+            else if (menu.SonShown)
+            {
+                menu.SonShown = false;
+                menu.OptionShown = true;
+            }
+            else if (menu.LangueShown)
+            {
+                menu.LangueShown = false;
+                menu.OptionShown = true;
             }
             else
-                menu.menuShown = !menu.menuShown;
+                menu.MenuShown = !menu.MenuShown;
         }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            inventaire.Cursors = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            inventaire.Cursors = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            inventaire.Cursors = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            inventaire.Cursors = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            inventaire.Cursors = 4;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            inventaire.Cursors = 5;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            inventaire.Cursors = 6;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            inventaire.Cursors = 7;
+        }
+        
+    }
+    void OnGUI()
+    {
+        if (!inventaire.Draggingitem && Event.current.button == 1 & Event.current.type == EventType.mouseDown)
+            inventaire.UsingItem();
     }
 }
--- Assets/Resources/Scripts/Menu.cs
+++ Assets/Resources/Scripts/Menu.cs
@@ -1,54 +1,137 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class Menu : MonoBehaviour {
-    public bool menuShown = false;
-    public bool optionShown = false;
+public class Menu : NetworkBehaviour {
+
+    public enum Language { French, English};
+
+    private bool menuShown = false;
+    private bool optionShown = false;
+    private bool sonShown = false;
+    private bool langueShown = false;
     private Inventory inventory;
     private GUISkin skin;
+    private Language langue;
 
     // Use this for initialization
     void Start()
     {
-        inventory = GetComponentInParent<Inventory>();
-        skin = Resources.Load<GUISkin>("Sprites/GUIskin/skin");
-    }
-
-    // Update is called once per frame
-    void Update()
-    { 
+        if (!isLocalPlayer)
+            return;
+        this.inventory = GetComponentInParent<Inventory>();
+        this.skin = Resources.Load<GUISkin>("Sprites/GUIskin/skin");
+        this.langue = (Language) PlayerPrefs.GetInt("langue", 0);
     }
 	void OnGUI()
         {
+        if (!isLocalPlayer)
+            return;
         if (menuShown)
         {
-            GUI.Box(new Rect(Screen.width /2-Screen.width/6, Screen.height / 2 -200,Screen.width/3,325), "MENU", skin.GetStyle("windows"));
-            MenuShown();
+            this.DrawMenu();
         }
-        if (optionShown)
-            OptionShown();
-
+        if (this.optionShown)
+            this.DrawOption();
+        if (this.langueShown)
+            this.DrawLangue();
+        if (this.sonShown)
+            this.DrawSon();
         }
 	
-    void MenuShown()
+    private void DrawMenu()
     {
-        if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 - 120, 80, 40), "Continuer",skin.GetStyle("button")))
+        GUI.Box(new Rect(Screen.width / 2 - Screen.width / 6, Screen.height / 2 - 200, Screen.width / 3, 325), "MENU", this.skin.GetStyle("windows"));
+        if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 - 120, 80, 40), this.langue == 0 ? "Continuer": "Resume" , this.skin.GetStyle("button")))
         {
-            menuShown = false;
+            this.menuShown = false;
         }
-        if (GUI.Button(new Rect(Screen.width/2 -40, Screen.height/2 -40,80,40), "Quitter",skin.GetStyle("button")))
+        if (GUI.Button(new Rect(Screen.width/2 -40, Screen.height/2 -40,80,40), this.langue == 0 ? "Quitter":"Quit", this.skin.GetStyle("button")))
         {
-            inventory.SaveInventory();
-            //quiter
+            this.inventory.SaveInventory();
+            Application.Quit();
         }
-        if (GUI.Button(new Rect(Screen.width/2 - 40,Screen.height/2 + 40, 80, 40), "Options", skin.GetStyle("button")))
+        if (GUI.Button(new Rect(Screen.width/2 - 40,Screen.height/2 + 40, 80, 40), "Options", this.skin.GetStyle("button")))
         {
-            menuShown = false;
-            optionShown = true;
+            this.menuShown = false;
+            this.optionShown = true;
         }
     }
-    void OptionShown()
+    private void DrawOption()
     {
+        GUI.Box(new Rect(Screen.width / 2 - Screen.width / 6, Screen.height / 2 - 200, Screen.width / 3, 325), "OPTIONS", this.skin.GetStyle("windows"));
+        if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 + 40, 80, 40), this.langue == 0 ? "Retour" : "Back", this.skin.GetStyle("button")))
+        {
+            this.menuShown = true;
+            this.optionShown = false;
+        }
+        if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 - 40, 80, 40), this.langue == 0 ? "Son":"Sound", this.skin.GetStyle("button")))
+        {
+            this.optionShown = false;
+            this.sonShown = true;
+        }
+        if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 - 120, 80, 40), this.langue == 0 ? "Langue": "Language", this.skin.GetStyle("button")))
+        {
+            this.optionShown = false;
+            this.langueShown = true;
+        }
+    }
+    private void DrawSon()
+    {
+        GUI.Box(new Rect(Screen.width / 2 - Screen.width / 6, Screen.height / 2 - 200, Screen.width / 3, 325), this.langue == 0 ? "SON" :"SOUND", this.skin.GetStyle("windows"));
+        if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 + 40, 80, 40), this.langue == 0 ? "Retour" : "Back", this.skin.GetStyle("button")))
+        {
+            this.optionShown = true;
+            this.sonShown = false;
+        }
+    }
+    private void DrawLangue()
+    {
+        GUI.Box(new Rect(Screen.width / 2 - Screen.width / 6, Screen.height / 2 - 200, Screen.width / 3, 325), this.langue == 0 ? "LANGUE":"LANGUAGE", this.skin.GetStyle("windows"));
+        if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 + 40, 80, 40), this.langue == 0 ? "Retour":"Back", this.skin.GetStyle("button")))
+        {
+            this.optionShown = true;
+            this.langueShown = false;
+        }
+        if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 - 40, 80, 40), this.langue == 0?"Français":"French", this.skin.GetStyle("button")))
+        {
+            PlayerPrefs.SetInt("langue", 0);
+            this.langue = Language.French;
+        }
+        if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 - 120, 80, 40), this.langue == 0 ? "Anglais":"English", this.skin.GetStyle("button")))
+        {
+            PlayerPrefs.SetInt("langue", 1);
+            this.langue = Language.English;
+        }
+    }
 
+    // Getters & Setters
+    public bool MenuShown
+    {
+        get { return this.menuShown; }
+        set { this.menuShown = value; }
+    }
+
+    public bool OptionShown
+    {
+        get { return this.optionShown; }
+        set { this.optionShown = value; }
+    }
+
+    public bool SonShown
+    {
+        get { return this.sonShown; }
+        set { this.sonShown = value; }
+    }
+
+    public bool LangueShown
+    {
+        get { return this.langueShown; }
+        set { this.langueShown = value; }
+    }
+
+    public Language Langue
+    {
+        get { return this.langue; }
     }
 }
