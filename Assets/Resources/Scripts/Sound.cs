91a5bc8308b8c5f4589d9272d03c7f7af5f61161
MrColorado <theoissarni@hotmail.fr>
2016-01-17 14:22:11 +0100

    Bruit de pas probleme saturation et declenchement

---
+++ Assets/Resources/Scripts/Sound.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour
+{
+
+    public AudioClip walk; 
+
+    private AudioSource source;
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f; 
+
+	// Use this for initialization
+	void Start ()
+    {
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (true) // Controller.isMoving je sais pas comment faire pour recuperer ce bool qui est initialisé dans controller
+        {
+            float vol = Random.Range(volLowRange, volHighRange);
+            source.PlayOneShot(walk, vol);
+        }
+    }
+}
+
+
+
+
+
