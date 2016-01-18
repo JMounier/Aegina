4aaf35b6b948e4413d0473d931da59239ab95712
flomonster <florian.amsallem@hotmail.fr>
2016-01-18 13:00:20 +0100

    Ajout de la classe, Entity, Biome, SpawnConfig.

---
+++ Assets/Resources/Scripts/Biome.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Utiliser cette classe pour creer un nouveau biome.
+/// </summary>
+public class Biome
+{
+    private int iD;
+    private SpawnConfig[] spwanConfiguration;
+    
+    // Constructor
+
+    public Biome()
+    {
+        this.iD = -1;
+        this.spwanConfiguration = new SpawnConfig[0];        
+    }
+
+    public Biome(int id, params Entity[] spawnableEntity)
+    {
+        this.iD = id;
+        this.spwanConfiguration = new SpawnConfig[spawnableEntity.Length];
+        float ratio = 1 / spawnableEntity.Length;
+        for (int i = 0; i < spawnableEntity.Length; i++)
+        {
+            this.spwanConfiguration[i] = new SpawnConfig(spawnableEntity[i], ratio);
+        }
+    }
+
+    public Biome(int id, params SpawnConfig[] spwanConfiguration)
+    {
+        this.iD = id;
+        this.spwanConfiguration = spwanConfiguration;
+        float sum = 0f;   
+        for (int i = 0; i < spwanConfiguration.Length; i++)
+        {
+            sum += spwanConfiguration[i].Ratio;
+        }
+
+        for (int i = 0; i < spwanConfiguration.Length; i++)
+        {
+            this.spwanConfiguration[i].Ratio /= sum;
+        }
+    }
+}
+
+/// <summary>
+/// Utiliser cette classe pour creer une nouvelle configuration de spawn.
+/// </summary>
+public class SpawnConfig
+{
+    private Entity e;
+    private float ratio;
+
+    // Cnostructor
+    public SpawnConfig()
+    {
+        this.e = null;
+        this.ratio = 0f;
+    }
+
+    public SpawnConfig(Entity e)
+    {
+        this.e = e;
+        this.ratio = 1f;
+    }
+
+    public SpawnConfig(Entity e, float ratio)
+    {
+        this.e = e;
+        this.ratio = ratio;
+    }
+
+    // Getter & Setter
+
+    /// <summary>
+    /// L'entite configure.
+    /// </summary>
+    public Entity E
+    {
+        get { return this.e; }
+        set { this.e = value; }
+    }
+
+    /// <summary>
+    /// Le ratio de spawn de l'entite.
+    /// </summary>
+    public float Ratio
+    {
+        get { return this.ratio; }
+        set { this.ratio = value; }
+    }
+}
---
+++ Assets/Resources/Scripts/Entity.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Utiliser cette classe pour creer une nouvelle entite.
+/// </summary>
+public class Entity
+{
+    protected int lifeMax;
+    protected int life;
+    protected GameObject prefab;
+
+    // Constructor
+    public Entity()
+    {
+        this.lifeMax = 0;
+        this.life = 0;
+        this.prefab = null;
+    }
+
+    public Entity(int life, GameObject prefab)
+    {
+        this.lifeMax = life;
+        this.life = life;
+        this.prefab = prefab;
+    }
+
+    // Methods
+
+    /// <summary>
+    /// Instancie l'entite dans le monde.
+    /// </summary>
+    public void Spawn()
+    {
+        GameObject.Instantiate(this.prefab, this.prefab.transform.position, this.prefab.transform.rotation);
+    }
+
+    /// <summary>
+    /// Instancie l'entite dans le monde avec une position.
+    /// </summary>
+    public void Spawn(Vector3 pos)
+    {
+        GameObject.Instantiate(this.prefab, pos, this.prefab.transform.rotation);
+    }
+
+    /// <summary>
+    /// Instancie l'entite dans le monde avec une rotation.
+    /// </summary>
+    public void Spawn(Quaternion rot)
+    {
+        GameObject.Instantiate(this.prefab, this.prefab.transform.position, rot);
+    }
+
+    /// <summary>
+    /// Instancie l'entite dans le monde avec une position et une rotation.
+    /// </summary>
+    public void Spawn(Vector3 pos, Quaternion rot)
+    {
+        GameObject.Instantiate(this.prefab, pos, rot);
+    }
+
+    // Getter & Setter
+
+    /// <summary>
+    /// La vie de l'entite.
+    /// </summary>
+    public int Life
+    {
+        get { return this.life; }
+        set { this.life = Mathf.Clamp(value, 0, this.lifeMax); }
+    }
+
+    /// <summary>
+    /// La vie maximum de l'entite.
+    /// </summary>
+    public int LifeMax
+    {
+        get { return this.lifeMax; }
+        set { this.lifeMax = (int) Mathf.Clamp(value, 0f, float.PositiveInfinity); }
+    }
+
+    /// <summary>
+    /// Le prefab de l'entite.
+    /// </summary>
+    public GameObject Prefab
+    {
+        get { return this.prefab; }
+        set { this.prefab = value; }
+    }
+}
