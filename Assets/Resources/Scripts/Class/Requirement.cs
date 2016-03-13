dbc0a4d08e6442cb95081a942efd14fcb5b4d3a9
JMounier <julien94.mounier@free.fr>
2016-03-13 19:04:27 +0100

    base des succès

---
+++ Assets/Resources/Scripts/Class/Requirement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+    public class Requirement
+    {
+
+    private int id;
+    private int statID;
+    private int value;
+    private char op;
+    private bool complit;
+
+    public Requirement(int id, int statID, char op, int value)
+    {
+		this.id = id;
+		this.statID = statID;
+        this.op = op;
+        this.value = value;
+        this.complit = false;
+    }
+
+	#region Methods
+
+	public void Check()
+	{
+		//Fixe Me
+	}
+
+	#endregion
+
+    #region Getters/Setters
+
+    public int Id
+    {
+        get { return this.id; }
+    }
+
+    public bool Complit
+    {
+        get { return this.complit; }
+    }
+    #endregion
+}
---
+++ Assets/Resources/Scripts/Class/Succes.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Succes {
+
+    private int id;
+    private Text description;
+    private Texture2D icon;
+    private bool achived;
+
+
+    private int nbCondition;
+    private Requirement[] requirements; 
+
+    private Succes[] sons;
+
+    public Succes(int id, Text description, Texture2D icon, Requirement[] requirements, params Succes[] sons)
+    {
+        this.id = id;
+        this.description = description;
+        this.icon = icon;
+        this.sons = sons;
+
+        // a voir comment on gere ça.
+        this.requirements = requirements;
+        this.nbCondition = requirements.Length;
+
+        this.achived = false;
+        this.Checkreq();
+    }
+
+    #region Methods
+
+    public void Checkreq()
+    {
+        if (!this.achived)
+        {
+            bool b = true;
+            foreach (Requirement req in requirements)
+            {
+                b = req.Complit;
+                if (!b)
+                    break;
+            }
+            this.achived = b;
+        }
+    }
+
+    #endregion
+
+    #region Getters/Setters
+    public int ID
+    {
+        get { return this.id; }
+    }
+
+    public int NbCondition
+    {
+        get { return this.nbCondition; }
+    }
+
+    public Text Description
+    {
+        get { return this.description; }
+    }
+
+    public Texture2D Icon
+    {
+        get { return this.icon; }
+    }
+
+    public bool Achived
+    {
+        get { return this.achived; }
+    }
+
+    public Succes[] Sons
+    {
+		get { return this.sons; }
+    }
+
+    #endregion
+}
