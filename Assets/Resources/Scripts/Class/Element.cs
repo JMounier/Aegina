ed0d22a6d153b0974668d1293a55499d0b1caf16
JMounier <julien94.mounier@free.fr>
2016-03-20 01:29:26 +0100

    fix destroi spawn

--- Assets/Resources/Scripts/Class/Element.cs
+++ Assets/Resources/Scripts/Class/Element.cs
@@ -44,7 +44,7 @@ public class Element : Entity
     {
         foreach (var dc in this.dropConfigs)
         {
-            Vector3 projection = new Vector3(Random.Range(-1f, 1f), Random.Range(2f, 3f), Random.Range(-1f, 1f));
+            Vector3 projection = new Vector3(Random.Range(-1f, 1f), Random.Range(1f, 2f), Random.Range(-1f, 1f));
             new Item(dc.I).Spawn(prefab.transform.position, projection, dc.Quantity);
         }
         base.Kill();
