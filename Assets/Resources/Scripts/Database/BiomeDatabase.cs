fafc5eb06700e5287afea04db3402578f87d980a
JMounier <julien94.mounier@free.fr>
2016-04-09 11:17:23 +0200

    les model tout bien comme y faut

--- Assets/Resources/Scripts/Database/BiomeDatabase.cs
+++ Assets/Resources/Scripts/Database/BiomeDatabase.cs
@@ -10,21 +10,27 @@ public static class BiomeDatabase
 
     // Biome forest
     public static readonly Biome Forest = new Biome(0, Resources.Load<Material>("Models/Islands/Materials/Forest"), Resources.Load<Material>("Models/Islands/Materials/Forest_Rock"),
-        new SpawnConfig(new Entity(), .5f), new SpawnConfig(EntityDatabase.Fir, 1), new SpawnConfig(EntityDatabase.StoneRock, 1),
-        new SpawnConfig(EntityDatabase.Oak, 2), new SpawnConfig(EntityDatabase.ForestFlower, 1), new SpawnConfig(EntityDatabase.Branch, 1),
-        new SpawnConfig(EntityDatabase.LittleRock, 1));
+        new SpawnConfig(new Entity(), 3f), new SpawnConfig(EntityDatabase.Fir, 5), new SpawnConfig(EntityDatabase.StoneRock, 3),
+        new SpawnConfig(EntityDatabase.CopperRock, 0.6f), new SpawnConfig(EntityDatabase.IronRock, 0.4f), new SpawnConfig(EntityDatabase.GoldRock, 0.2f),
+        new SpawnConfig(EntityDatabase.MithrilRock, 0.2f), new SpawnConfig(EntityDatabase.FloatiumRock, 0.1f), new SpawnConfig(EntityDatabase.SunkiumRock, 0.05f),
+        new SpawnConfig(EntityDatabase.Oak, 6), new SpawnConfig(EntityDatabase.ForestFlower, 4), new SpawnConfig(EntityDatabase.Branch, 4),
+        new SpawnConfig(EntityDatabase.LittleRock, 2));
 
 
     // Biome Desert
     public static readonly Biome Desert = new Biome(1, Resources.Load<Material>("Models/Islands/Materials/Desert"), Resources.Load<Material>("Models/Islands/Materials/Desert_Rock"),
-        new SpawnConfig(new Entity(), 1f), new SpawnConfig(EntityDatabase.Cactus, 1), new SpawnConfig(EntityDatabase.StoneRock, 1),
-        new SpawnConfig(EntityDatabase.SmallCactus, 1.5f), new SpawnConfig(EntityDatabase.Branch, 2), new SpawnConfig(EntityDatabase.LittleRock, 1));
+        new SpawnConfig(new Entity(), 3), new SpawnConfig(EntityDatabase.Cactus, 2), new SpawnConfig(EntityDatabase.StoneRock, 4),
+        new SpawnConfig(EntityDatabase.CopperRock, 0.6f), new SpawnConfig(EntityDatabase.IronRock, 0.4f), new SpawnConfig(EntityDatabase.GoldRock, 0.2f),
+        new SpawnConfig(EntityDatabase.MithrilRock, 0.2f), new SpawnConfig(EntityDatabase.FloatiumRock, 0.1f), new SpawnConfig(EntityDatabase.SunkiumRock, 0.05f),
+        new SpawnConfig(EntityDatabase.SmallCactus, 2), new SpawnConfig(EntityDatabase.Branch, 2), new SpawnConfig(EntityDatabase.LittleRock, 2));
 
 
     // Biome Ice
     public static readonly Biome Ice = new Biome(2, Resources.Load<Material>("Models/Islands/Materials/Ice"), Resources.Load<Material>("Models/Islands/Materials/Ice_Rock"),
-        new SpawnConfig(new Entity(), .5f), new SpawnConfig(EntityDatabase.SnowFir, 3), new SpawnConfig(EntityDatabase.StoneRock, 1),
-        new SpawnConfig(EntityDatabase.SnowOak, 2), new SpawnConfig(EntityDatabase.IceFlower, 1), new SpawnConfig(EntityDatabase.LittleRock, 1));
+        new SpawnConfig(new Entity(), 3), new SpawnConfig(EntityDatabase.SnowFir, 5), new SpawnConfig(EntityDatabase.StoneRock, 3),
+        new SpawnConfig(EntityDatabase.CopperRock, 0.6f), new SpawnConfig(EntityDatabase.IronRock, 0.4f), new SpawnConfig(EntityDatabase.GoldRock, 0.2f),
+        new SpawnConfig(EntityDatabase.MithrilRock, 0.2f), new SpawnConfig(EntityDatabase.FloatiumRock, 0.1f), new SpawnConfig(EntityDatabase.SunkiumRock, 0.05f),
+        new SpawnConfig(EntityDatabase.SnowOak, 7), new SpawnConfig(EntityDatabase.IceFlower, 5), new SpawnConfig(EntityDatabase.LittleRock, 3));
 
 
     /// <summary>
