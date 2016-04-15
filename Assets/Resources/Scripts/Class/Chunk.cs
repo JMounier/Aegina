5ff85614c858cd89b4eb4919586ea124337522ee
flomonster <florian.amsallem@hotmail.fr>
2016-04-15 17:36:10 +0200

    Fix spawn mob and defocus

--- Assets/Resources/Scripts/Class/Chunk.cs
+++ Assets/Resources/Scripts/Class/Chunk.cs
@@ -99,11 +99,12 @@ public class Chunk : Entity
         }
 
         //generate Mobs
+        
         foreach (Mob mob in EntityDatabase.Mobs)
         {
             for (int i = 0; i < mob.GroupSize; i++)
             {
-                Vector3 pos = new Vector3(UnityEngine.Random.Range(-Size / 2 + x * Size, Size / 2 + y * Size), 7, UnityEngine.Random.Range(-Size / 2 + x * Size, Size / 2 + y * Size));               
+                Vector3 pos = new Vector3(UnityEngine.Random.Range(-Size / 2 + x * Size, Size / 2 + x * Size), 7, UnityEngine.Random.Range(-Size / 2 + y * Size, Size / 2 + y * Size));               
                 if (Graph.isValidPosition(pos))
                     new Mob(mob).Spawn(pos, Prefab.transform.FindChild("Mob"));
             }
