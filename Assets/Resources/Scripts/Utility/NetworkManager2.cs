0b131f5b7534f6b57c7aa2392b1d3329c12ec5cb
nokiiam <romain.mounier@orange.fr>
2016-04-18 09:55:39 +0200

    changement dans le syncCore pour de meilleur performance et modification du networkManager pour une récupération AUTOMATIQUE des prefabs

---
+++ Assets/Resources/Scripts/Utility/NetworkManager2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine.Networking.Match;
+using System.IO;
+using System.Linq;
+
+namespace UnityEngine.Networking
+{
+	public class NetworkManager2 : NetworkManager
+	{
+
+		// Use this for initialization
+		void Start ()
+		{
+			spawnPrefabs.AddRange (Resources.LoadAll<GameObject> ("Prefabs"));
+		}
+	}
+}
+
