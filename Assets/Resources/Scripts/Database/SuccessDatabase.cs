281f29e58bb766f55a997792c09aae6e66553afe
nokiiam <romain.mounier@orange.fr>
2016-04-23 16:02:43 +0200

    Ajout de la fonction Pop-up des succès. Il reste une modification à faire sur le GUIskin mais je l'ai pas faite pour éviter les conflit

--- Assets/Resources/Scripts/Database/SuccessDatabase.cs
+++ Assets/Resources/Scripts/Database/SuccessDatabase.cs
@@ -1,7 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SuccessDatabase
 {
     public static readonly Success Root = new Success(0, TextDatabase.Apple, ItemDatabase.Workbench, new Requirement.Requirements[1] { Requirement.Requirements.Tuto }, 0);
+	public static IEnumerable<Success> Success {
+		get { 
+			yield return Root;
+		}
+	}
+	public static Success Find(int id){
+		foreach (Success success in Success) {
+			if (success.ID==id) 
+				return success;
+		}
+		throw new System.Exception ("Succes not found");
+	}
 }
