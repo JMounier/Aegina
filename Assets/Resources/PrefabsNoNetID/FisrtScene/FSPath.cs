4c8a7b74fe830a01c5af10706831f0e6237d867b
Julien Mounier <julien94.mounier@free.fr>
2016-04-24 21:42:58 +0200

    First scene2 (#277)
    * La nouvelle map sans aucun networck id
    
    * mon systeme de path fonctionne reste plus qu'a posser les balises
    
    * Le train est en marche
    
    * vous me faites chiez cordialement

---
+++ Assets/Resources/PrefabsNoNetID/FisrtScene/FSPath.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum FaceSide {None, forward, left , right, backward};
+
+public class FSPath : MonoBehaviour {
+	
+
+	private FaceSide dir = FaceSide.forward;
+	[SerializeField]
+	private float speed = 0.1f;
+	[SerializeField]
+	private GameObject nextStep;
+
+	#region Getters/Setters
+
+	/// <summary>
+	/// Gets the direction the cam should face.
+	/// </summary>
+	/// <value>The dir.</value>
+	public FaceSide Dir{
+		get{ return this.dir;}
+	}
+
+	/// <summary>
+	/// Gets the speed.
+	/// </summary>
+	/// <value>The speed.</value>
+	public float Speed{
+		get{ return this.speed;}
+	}
+
+	/// <summary>
+	/// Gets the next step.
+	/// </summary>
+	/// <value>The next step.</value>
+	public GameObject NextStep{
+		get{ return this.nextStep;}
+	}
+	#endregion
+}
--- Assets/Resources/Scripts/Networking/FirstScene.cs
+++ Assets/Resources/Scripts/Networking/FirstScene.cs
@@ -3,33 +3,58 @@ using System.Collections;
 
 public class FirstScene : MonoBehaviour
 {
+	
+	[SerializeField]
     private Light sun;
-    private Camera cam;
-    private AudioSource source;
+
+	private int diameter = 100;
+	private float actual_time;
+	private float cycleTime = 120;
+
+
+
+	[SerializeField]
+	private Camera cam;
+	[SerializeField]
+	private float camSpeed = 0.05f;
+
+	[SerializeField]
+	private GameObject fistStep;
+	[SerializeField]
+	private float acceptance = 1;
+
+	private GameObject step;
+
+
+
+	[SerializeField]
+	private AudioSource source;
+
     private AudioClip clipMenu;
     private AudioClip clipButton;
     private float cdMusic;
     private float volume;
 
-    private int diameter = 100;
-    private float actual_time;
-    private float cycleTime = 120;
+
     // Use this for initialization
     void Start()
     {
         // D/N
-        this.sun = gameObject.GetComponentInChildren<Light>();
+        //this.sun = gameObject.GetComponentInChildren<Light>();
         this.sun.gameObject.transform.TransformPoint(sun.transform.position);
 
         this.actual_time = 0f;
         this.cdMusic = 0f;
         this.volume = PlayerPrefs.GetFloat("Sound_intensity", 0.1f);
 
-        // Camera
-        this.cam = gameObject.GetComponentInChildren<Camera>();
+		// Camera and Path
+		this.cam.transform.position = this.fistStep.transform.position;
+		this.step = this.fistStep.GetComponent<FSPath> ().NextStep;
+		//this.camSpeed = this.step.GetComponent<FSPath> ().Speed;
+		this.cam.transform.LookAt (this.step.transform);
 
-        this.source = gameObject.GetComponentInChildren<AudioSource>();
-        this.source.volume = this.volume;
+		// Audio Source
+		this.source.volume = this.volume;
         this.clipMenu = Resources.Load<AudioClip>("Sounds/Music/Menu");
         this.clipButton = Resources.Load<AudioClip>("Sounds/Button/Button");
     }
@@ -45,17 +70,31 @@ public class FirstScene : MonoBehaviour
         this.sun.transform.position = Orbit(this.actual_time);
         this.sun.transform.LookAt(gameObject.transform);
 
-        // Camera
-        cam.transform.LookAt(gameObject.transform);
-        cam.transform.Translate(Vector3.left / 10);
-
-        // Sound 
+       // Sound 
         this.cdMusic -= Time.deltaTime;
         if (this.cdMusic <= 0)
         {
             this.source.PlayOneShot(this.clipMenu, 1f);
             this.cdMusic = 112f;
-        }            
+        }  
+
+		// Camera
+
+		cam.transform.Translate(Vector3.forward * this.camSpeed);
+
+		//choose the rot;
+		Quaternion lastrot = this.cam.transform.rotation;
+		this.cam.transform.LookAt (this.step.transform);
+		Quaternion newrot = this.cam.transform.rotation;
+		this.cam.transform.rotation = Quaternion.Lerp (lastrot, newrot, 0.01f);
+
+		if (Vector3.Distance (this.cam.transform.position, this.step.transform.position) <= this.acceptance) {
+			this.step = this.step.GetComponent<FSPath> ().NextStep;
+			if (this.step.GetComponent<FSPath> () == null)
+				this.step = this.fistStep;
+			//this.camSpeed = this.step.GetComponent<FSPath> ().Speed;
+		}
+
     }
 
 
