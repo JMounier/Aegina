97345dfa2ab5c87707290a929c5fd2774336b534
JMounier <julien94.mounier@free.fr>
2016-04-26 18:19:24 +0200

    les liens entre les succes

--- Assets/Resources/Scripts/Networking/Save.cs
+++ Assets/Resources/Scripts/Networking/Save.cs
@@ -306,42 +306,38 @@ public class ChunkSave
         this.path = pathChunk + x.ToString() + "_" + y.ToString();
         this.cristal = new float[8];
 
-        if (File.Exists(this.path))
-        {
-            string[] lines = File.ReadAllLines(this.path);
-            foreach (string str in lines[0].Split('|'))
-            {
-                int id;
-                if (int.TryParse(str, out id))
-                    this.idSave.Add(id);
-            }
-            foreach (string str in lines[1].Split('|'))
-            {
-                string[] components = str.Split(':');
-                int id;
-                if (int.TryParse(components[0], out id))
-                {
-                    float posX = float.Parse(components[1]);
-                    float posY = float.Parse(components[2]);
-                    float posZ = float.Parse(components[3]);
-                    float rotX = float.Parse(components[4]);
-                    float rotY = float.Parse(components[5]);
-                    float rotZ = float.Parse(components[6]);
-                    this.workTops.Add(new Triple<Element, Vector3, Vector3>(EntityDatabase.Find(id) as Element,
-                        new Vector3(posX, posY, posZ), new Vector3(rotX, rotY, rotZ)));
-                }
-            }
-            if (lines.Length > 2)
-            {
-                string[] cristalCaract = lines[2].Split('|');
-                for (int i = 0; i < 6; i++)
-                    this.cristal[i] = int.Parse(cristalCaract[i]);
-                for (int i = 6; i < 8; i++)
-                    this.cristal[i] = float.Parse(cristalCaract[i]);
-            }
-        }
-        else
-            File.Create(this.path);
+		if (File.Exists (this.path)) {
+			string[] lines = File.ReadAllLines (this.path);
+			foreach (string str in lines[0].Split('|')) {
+				int id;
+				if (int.TryParse (str, out id))
+					this.idSave.Add (id);
+			}
+			foreach (string str in lines[1].Split('|')) {
+				string[] components = str.Split (':');
+				int id;
+				if (int.TryParse (components [0], out id)) {
+					float posX = float.Parse (components [1]);
+					float posY = float.Parse (components [2]);
+					float posZ = float.Parse (components [3]);
+					float rotX = float.Parse (components [4]);
+					float rotY = float.Parse (components [5]);
+					float rotZ = float.Parse (components [6]);
+					this.workTops.Add (new Triple<Element, Vector3, Vector3> (EntityDatabase.Find (id) as Element,
+						new Vector3 (posX, posY, posZ), new Vector3 (rotX, rotY, rotZ)));
+				}
+			}
+			if (lines.Length > 2) {
+				string[] cristalCaract = lines [2].Split ('|');
+				for (int i = 0; i < 6; i++)
+					this.cristal [i] = int.Parse (cristalCaract [i]);
+				for (int i = 6; i < 8; i++)
+					this.cristal [i] = float.Parse (cristalCaract [i]);
+			}
+		} else {
+			File.Create (this.path);
+			this.cristal [5] = 1000;
+		}
     }
 
     /// <summary>
