4da170b3b2ac3b4202f2edad38a6c93dba98b67d
flomonster <florian.amsallem@hotmail.fr>
2016-05-28 14:01:00 +0200

    Starting chest

---
+++ Assets/Resources/Scripts/Networking/SyncChest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SyncChest : SyncElement
+{
+    private ItemStack[,] content;
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        this.content = new ItemStack[3, 3] { 
+            { new ItemStack(), new ItemStack(), new ItemStack() },
+            { new ItemStack(), new ItemStack(), new ItemStack() },
+            { new ItemStack(), new ItemStack(), new ItemStack() } };
+        base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
