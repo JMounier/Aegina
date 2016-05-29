7e512be54630458e97393157213b3ec8ab650ca9
nokiiam <romain.mounier@orange.fr>
2016-05-29 02:16:24 +0200

    Fenêtre d'aide

--- Assets/Resources/Scripts/Player/Menu.cs
+++ Assets/Resources/Scripts/Player/Menu.cs
@@ -10,13 +10,16 @@ public class Menu : NetworkBehaviour
     private bool optionShown = false;
     private bool sonShown = false;
     private bool langueShown = false;
-	private bool controlShown = false;
+    private bool controlShown = false;
+    private bool helpageShown = false;
     private Inventory inventory;
     private GUISkin skin;
     private Controller controller;
     private NetworkManager NM;
     private Sound soundAudio;
-	private Camera camera;
+    private Camera camera;
+    private Texture2D[] HelpPage;
+    private int page;
 
     // Use this for initialization
     void Start()
@@ -34,7 +37,9 @@ public class Menu : NetworkBehaviour
         this.height = Screen.height / 30;
         this.spacing = this.height * 2;
         this.soundAudio = GetComponent<Sound>();
-		this.camera = this.GetComponentInChildren<Camera> ();
+        this.camera = this.GetComponentInChildren<Camera>();
+        this.page = -1;
+        this.HelpPage = Resources.LoadAll<Texture2D>("Sprites/AideDeJeu");
     }
 
     void Update()
@@ -49,16 +54,18 @@ public class Menu : NetworkBehaviour
     {
         if (!isLocalPlayer)
             return;
-		if (menuShown)
-			this.DrawMenu ();
-		else if (this.optionShown)
-			this.DrawOption ();
-		else if (this.langueShown)
-			this.DrawLangue ();
-		else if (this.sonShown)
-			this.DrawSon ();
-		else if (this.controlShown)
-			this.DrawControl ();
+        if (menuShown)
+            this.DrawMenu();
+        else if (this.optionShown)
+            this.DrawOption();
+        else if (this.langueShown)
+            this.DrawLangue();
+        else if (this.sonShown)
+            this.DrawSon();
+        else if (this.controlShown)
+            this.DrawControl();
+        else if (this.helpageShown)
+            this.DrawHelp();
     }
 
     /// <summary>
@@ -82,7 +89,7 @@ public class Menu : NetworkBehaviour
             this.soundAudio.PlaySound(AudioClips.Button, 1f);
         }
 
-        if (GUI.Button(new Rect(this.posX, this.posY + this.spacing * 2, this.width, this.height), TextDatabase.Quit.GetText(), skin.GetStyle("button")))
+        if (GUI.Button(new Rect(this.posX, this.posY + this.spacing * 3, this.width, this.height), TextDatabase.Quit.GetText(), skin.GetStyle("button")))
         {
             this.menuShown = false;
             this.soundAudio.PlaySound(AudioClips.Button, 1f);
@@ -93,6 +100,12 @@ public class Menu : NetworkBehaviour
             }
             else
                 CmdDisconnect();
+        }
+        if (GUI.Button(new Rect(this.posX, this.posY + this.spacing * 2, this.width, this.height), TextDatabase.Help.GetText(), skin.GetStyle("button")))
+        {
+            this.menuShown = false;
+            this.helpageShown = true;
+            this.soundAudio.PlaySound(AudioClips.Button, 1f);
         }
     }
 
@@ -140,11 +153,11 @@ public class Menu : NetworkBehaviour
             this.optionShown = false;
             this.soundAudio.PlaySound(AudioClips.Button, 1f);
         }
-		if (GUI.Button (new Rect (this.posX, this.posY + this.spacing * 2, this.width, this.height), "Camera", skin.GetStyle ("button"))) 
-		{
-			this.optionShown = false;
-			this.controlShown = true;
-			this.soundAudio.PlaySound (AudioClips.Button, 1f);
+        if (GUI.Button(new Rect(this.posX, this.posY + this.spacing * 2, this.width, this.height), "Camera", skin.GetStyle("button")))
+        {
+            this.optionShown = false;
+            this.controlShown = true;
+            this.soundAudio.PlaySound(AudioClips.Button, 1f);
             this.soundAudio.Volume = PlayerPrefs.GetFloat("Sensitivity", 4f);
             this.camera.farClipPlane = PlayerPrefs.GetFloat("FieldOfView", 60f);
         }
@@ -205,32 +218,64 @@ public class Menu : NetworkBehaviour
         }
     }
 
-	private void DrawControl()
-	{
-		GUI.Box(new Rect(Screen.width / 4, Screen.height / 6, Screen.width / 2, Screen.width / 12.8f), "", this.skin.GetStyle("Aegina"));
-		GUI.Box (new Rect (this.posX - this.spacing, this.posY - this.spacing, this.width + 2 * this.spacing, this.height + 8 * this.spacing), "", skin.GetStyle ("Inventory"));
-		GUI.Box (new Rect (this.posX, this.posY, this.width, this.height),TextDatabase.Sensibility.GetText(),skin.GetStyle("Description"));
-		this.controller.Sensitivity = GUI.HorizontalSlider(new Rect(this.posX, this.posY+this.spacing, this.width, this.height), this.controller.Sensitivity, 0.1f, 10f, this.skin.GetStyle("horizontalslider"), this.skin.GetStyle("horizontalsliderthumb"));
+    private void DrawControl()
+    {
+        GUI.Box(new Rect(Screen.width / 4, Screen.height / 6, Screen.width / 2, Screen.width / 12.8f), "", this.skin.GetStyle("Aegina"));
+        GUI.Box(new Rect(this.posX - this.spacing, this.posY - this.spacing, this.width + 2 * this.spacing, this.height + 8 * this.spacing), "", skin.GetStyle("Inventory"));
+        GUI.Box(new Rect(this.posX, this.posY, this.width, this.height), TextDatabase.Sensibility.GetText(), skin.GetStyle("Description"));
+        this.controller.Sensitivity = GUI.HorizontalSlider(new Rect(this.posX, this.posY + this.spacing, this.width, this.height), this.controller.Sensitivity, 0.1f, 10f, this.skin.GetStyle("horizontalslider"), this.skin.GetStyle("horizontalsliderthumb"));
 
-		if (GUI.Button(new Rect(this.posX, this.posY + 4*this.spacing, this.width, this.height), TextDatabase.Validate.GetText(), this.skin.GetStyle("button")))
-		{
-			this.optionShown = true;
-			this.controlShown = false;
-			PlayerPrefs.SetFloat("Sensitivity", this.soundAudio.Volume);
-			PlayerPrefs.SetFloat("FieldOfView", this.soundAudio.Volume);
-			this.soundAudio.PlaySound(AudioClips.Button, 1f);
-		}
-		GUI.Box (new Rect (this.posX, this.posY + 2*this.spacing, this.width, this.height),TextDatabase.FieldOfView.GetText(),skin.GetStyle("Description"));
-		this.camera.farClipPlane = GUI.HorizontalSlider(new Rect(this.posX, this.posY+3*this.spacing, this.width, this.height), this.camera.farClipPlane, 10f, 200f, this.skin.GetStyle("horizontalslider"), this.skin.GetStyle("horizontalsliderthumb"));
-		if (GUI.Button(new Rect(this.posX, this.posY + this.spacing * 5, this.width, this.height), TextDatabase.Back.GetText(), this.skin.GetStyle("button")))
-		{
-			this.optionShown = true;
-			this.controlShown = false;
-			this.soundAudio.Volume = PlayerPrefs.GetFloat("Sensitivity", 4f);
-			this.camera.farClipPlane = PlayerPrefs.GetFloat("FieldOfView", 60f);
-			this.soundAudio.PlaySound(AudioClips.Button, 1f);
-		}
-	}
+        if (GUI.Button(new Rect(this.posX, this.posY + 4 * this.spacing, this.width, this.height), TextDatabase.Validate.GetText(), this.skin.GetStyle("button")))
+        {
+            this.optionShown = true;
+            this.controlShown = false;
+            PlayerPrefs.SetFloat("Sensitivity", this.soundAudio.Volume);
+            PlayerPrefs.SetFloat("FieldOfView", this.soundAudio.Volume);
+            this.soundAudio.PlaySound(AudioClips.Button, 1f);
+        }
+        GUI.Box(new Rect(this.posX, this.posY + 2 * this.spacing, this.width, this.height), TextDatabase.FieldOfView.GetText(), skin.GetStyle("Description"));
+        this.camera.farClipPlane = GUI.HorizontalSlider(new Rect(this.posX, this.posY + 3 * this.spacing, this.width, this.height), this.camera.farClipPlane, 10f, 200f, this.skin.GetStyle("horizontalslider"), this.skin.GetStyle("horizontalsliderthumb"));
+        if (GUI.Button(new Rect(this.posX, this.posY + this.spacing * 5, this.width, this.height), TextDatabase.Back.GetText(), this.skin.GetStyle("button")))
+        {
+            this.optionShown = true;
+            this.controlShown = false;
+            this.soundAudio.Volume = PlayerPrefs.GetFloat("Sensitivity", 4f);
+            this.camera.farClipPlane = PlayerPrefs.GetFloat("FieldOfView", 60f);
+            this.soundAudio.PlaySound(AudioClips.Button, 1f);
+        }
+    }
+    private void DrawHelp()
+    {
+        if (page == -1)
+        {
+            GUI.Box(new Rect(Screen.width / 4, Screen.height / 6, Screen.width / 2, Screen.width / 12.8f), "", this.skin.GetStyle("Aegina"));
+            if (GUI.Button(new Rect(this.posX, this.posY, this.width, this.height), TextDatabase.CombatHelp.GetText(), this.skin.GetStyle("button")))
+                this.page = Text.GetLanguage() == SystemLanguage.English ? 0 : 3;
+            if (GUI.Button(new Rect(this.posX, this.posY + this.spacing * 1, this.width, this.height), "Cristal", this.skin.GetStyle("button")))
+                this.page = 1 + (Text.GetLanguage() == SystemLanguage.English ? 0 : 3);
+            if (GUI.Button(new Rect(this.posX, this.posY + this.spacing * 2, this.width, this.height), TextDatabase.InventaireHelp.GetText(), this.skin.GetStyle("button")))
+                this.page = 2 + (Text.GetLanguage() == SystemLanguage.English ? 0 : 3);
+            if (GUI.Button(new Rect(this.posX, this.posY + this.spacing * 3, this.width, this.height), TextDatabase.Back.GetText(), this.skin.GetStyle("button")))
+            {
+                this.optionShown = true;
+                this.helpageShown = false;
+                this.soundAudio.PlaySound(AudioClips.Button, 1f);
+            }
+        }
+        else
+        {
+            Rect rect = new Rect(Screen.width / 4, Screen.height / 7, Screen.width / 2, Screen.height / 2 + 2*this.spacing);
+            GUI.Box(rect, "", skin.GetStyle("inventory"));
+            rect.x += Screen.width / 50;
+            rect.y += Screen.height / 20;
+            rect.width -= Screen.width / 25;
+            rect.height -= Screen.height / 10;
+            GUI.DrawTexture(rect, HelpPage[page]);
+            rect = new Rect(Screen.width / 2 -Screen.width/20, 3 * Screen.height / 4 + this.spacing / 2, Screen.width / 10, this.spacing);
+            if (GUI.Button(rect, TextDatabase.Back.GetText(), skin.GetStyle("button")))
+                this.page = -1; 
+        }
+    }
 
     // Getters & Setters
 
@@ -269,9 +314,21 @@ public class Menu : NetworkBehaviour
         get { return this.langueShown; }
         set { this.langueShown = value; }
     }
-	public bool ControlShown
-	{
-		get{ return this.controlShown;}
-		set { this.controlShown = value;}
-	}
+    public bool ControlShown
+    {
+        get { return this.controlShown; }
+        set { this.controlShown = value; }
+    }
+
+    public bool Helpshown
+    {
+        get { return this.helpageShown; }
+        set { this.helpageShown = value; }
+    }
+
+    public int Page
+    {
+        get { return this.page; }
+        set { this.page = value; }
+    }
 }
