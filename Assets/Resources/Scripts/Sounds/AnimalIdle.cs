7038feb3e3114896685bec8d4eb2c4a2e16e0256
MrColorado <theoissarni@hotmail.fr>
2016-06-14 18:52:55 +0200

    Anime jump

---
+++ Assets/Resources/Scripts/Sounds/AnimalIdle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalIdle : StateMachineBehaviour {
+
+    private float cd;
+    [SerializeField]
+    private float probability;
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (!(cd > 0))
+        {
+            if (Random.Range(0f, 1f) < probability)
+            {
+                string mobName = animator.gameObject.name.Replace("(Clone)", "");
+                animator.gameObject.GetComponentInParent<AudioSource>().PlayOneShot(Resources.Load<AudioClip>("Sounds/Mob/" + mobName + "/Idle"), 1);
+            }
+            cd = Random.Range(1f, 2f);
+        }
+        else
+            cd -= Time.deltaTime;
+    }
+
+}
